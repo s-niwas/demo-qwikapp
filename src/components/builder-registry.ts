9806fbea1341a491711e90b8c30073e1943d95e3
Niwas S <niwass@Niwass-MacBook-Air.local>
2023-10-03 18:29:48 +0530

    fix(ui): ui polish in footer

--- src/components/builder-registry.ts
+++ src/components/builder-registry.ts
@@ -1,53 +1,16 @@
 import type { RegisteredComponent } from "@builder.io/sdk-qwik";
-import Feature from "./feature/feature";
-import Footer from "./footer/footer";
-import Header from "./header/header";
 import Aboutus from "./aboutus/aboutus";
-import Testimonials from "./testimonials/testimonials";
-import FAQ from "./faq/faq";
 import Clients from "./clients/clients";
 import Contact from "./contact/contact";
+import FAQ from "./faq/faq";
+import Feature from "./feature/feature";
+import Footer from "./footer/footer";
 import Form from "./contact/form";
+import Header from "./header/header";
+import Testimonials from "./testimonials/testimonials";
+
 
 export const CUSTOM_COMPONENTS: RegisteredComponent[] = [
-  {
-    component: Feature,
-    name: "Feature",
-    inputs: [
-      {
-        name: "image",
-        type: "file",
-        allowedFileTypes: ["svg"],
-      },
-      {
-        name: "title",
-        type: "string",
-      },
-      {
-        name: "description",
-        type: "string",
-      },
-      {
-        name: "buttonText",
-        type: "string",
-      },
-    ],
-  },
-  {
-    component: Footer,
-    name: "Footer",
-    inputs: [],
-  },
-  {
-    component: Header,
-    name: "Header",
-    inputs: [
-      {
-        name: "logo",
-        type: "file",
-      },
-    ],
-  },
   {
     component: Aboutus,
     name: "About us",
@@ -75,42 +38,6 @@ export const CUSTOM_COMPONENTS: RegisteredComponent[] = [
       },
       {
         name: "buttonText",
-        type: "string",
-      },
-    ],
-  },
-  {
-    component: FAQ,
-    name: "FAQ",
-    inputs: [
-      {
-        name: "question",
-        type: "string",
-      },
-      {
-        name: "answer",
-        type: "longText",
-      },
-    ],
-  },
-  {
-    component: Testimonials,
-    name: "Testimonial",
-    inputs: [
-      {
-        name: "authorImage",
-        type: "file",
-      },
-      {
-        name: "authorName",
-        type: "string",
-      },
-      {
-        name: "authorRole",
-        type: "string",
-      },
-      {
-        name: "description",
         type: "string",
       },
     ],
@@ -148,8 +75,111 @@ export const CUSTOM_COMPONENTS: RegisteredComponent[] = [
     ],
   },
   {
+    component: FAQ,
+    name: "FAQ",
+    inputs: [
+      {
+        name: "question",
+        type: "string",
+      },
+      {
+        name: "answer",
+        type: "longText",
+      },
+    ],
+  },
+  {
+    component: Feature,
+    name: "Feature",
+    inputs: [
+      {
+        name: "image",
+        type: "file",
+        allowedFileTypes: ["svg"],
+      },
+      {
+        name: "title",
+        type: "string",
+      },
+      {
+        name: "description",
+        type: "string",
+      },
+      {
+        name: "buttonText",
+        type: "string",
+      },
+    ],
+  },
+  {
+    component: Footer,
+    name: "Footer",
+    inputs: [
+      {
+        name: "logo",
+        type: "file",
+      },
+      {
+        name: "ayroui",
+        type: "file",
+      },
+      {
+        name: "ecommerce",
+        type: "file",
+      },
+      {
+        name: "graygrids",
+        type: "file",
+      },
+      {
+        name: "lineicons",
+        type: "file",
+      },
+      {
+        name: "uideck",
+        type: "file",
+      },
+      {
+        name: "tailgrids",
+        type: "file",
+      },
+    ],
+  },
+  {
     component: Form,
     name: "Form",
     inputs: [],
+  },
+  {
+    component: Header,
+    name: "Header",
+    inputs: [
+      {
+        name: "logo",
+        type: "file",
+      },
+    ],
+  },
+  {
+    component: Testimonials,
+    name: "Testimonial",
+    inputs: [
+      {
+        name: "authorImage",
+        type: "file",
+      },
+      {
+        name: "authorName",
+        type: "string",
+      },
+      {
+        name: "authorRole",
+        type: "string",
+      },
+      {
+        name: "description",
+        type: "string",
+      },
+    ],
   },
 ];
